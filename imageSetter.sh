f6270f8f329fe8d5ac239e1de54443a1a1466347
BotNikos <boss.nikitos1212@mail.ru>
2024-02-23 12:52:17 +0300

    deleted one wallpaer, renamed all

--- imageSetter.sh
+++ imageSetter.sh
@@ -1,8 +1,9 @@
 path=~/Wallpapers/
 pngs=$(ls $path/*.png | wc -l)
-jpgs=$(ls $path/*.jpg | wc -l)
+# jpgs=$(ls $path/*.jpg | wc -l)
 
-imagesCount=$(expr $pngs + $jpgs)
+# imagesCount=$(expr $pngs + $jpgs)
+imagesCount=$(expr $pngs)
 
 feh --bg-fill $path/$((1 + $RANDOM % $imagesCount)).* --bg-fill $path/$((1 + $RANDOM % $imagesCount)).*
 
