23a129032cbc8021810299f302f01b7d83d0aa45
BotNikos <boss.nikitos1212@mail.ru>
2024-09-06 10:57:20 +0300

    changed background utility to swww

--- imageSetter.sh
+++ imageSetter.sh
@@ -5,5 +5,10 @@ pngs=$(ls $path/*.png | wc -l)
 # imagesCount=$(expr $pngs + $jpgs)
 imagesCount=$(expr $pngs)
 
-feh --bg-fill $path/$((1 + $RANDOM % $imagesCount)).* --bg-fill $path/$((1 + $RANDOM % $imagesCount)).*
+# For Xorg (requires feh)
+# feh --bg-fill $path/$((1 + $RANDOM % $imagesCount)).* --bg-fill $path/$((1 + $RANDOM % $imagesCount)).* 
+
+# For wayland (requires swww)
+swww img -o HDMI-A-1 $path/$((1 + $RANDOM % $imagesCount)).* --transition-step 10 --transition-fps 60 --transition-type random
+swww img -o HDMI-A-2 $path/$((1 + $RANDOM % $imagesCount)).* --transition-step 10 --transition-fps 60 --transition-type random 
 
